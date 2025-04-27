45eba6cdc1dfb22cc2cca8c0a23bd00bc65ebc09
mamata0007 <sahoomamta11@gmail.com>
2025-04-27 11:15:12 +0530

    added

---
+++ src/Java8_Program/Findfrequency.java
@@ -0,0 +1,16 @@
+package Java8_Program;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Findfrequency {
+
+	public static void main(String[] args) {
+String s="java is a concept of the day";
+Map<Character,Long> results=s.chars()
+.mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+System.out.println(results);
+	}
+
+}
